57db071f75209a60917fb9e1c867349ac9bae1b0
Stuart Willis <scwillis@hotmail.co.uk>
2018-01-26 15:30:35 +0000

    add: new house form, post route to house page

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+
+ var housing = [
+        {name:"1 bed apartment in Barca", image:"https://images.pexels.com/photos/275484/pexels-photo-275484.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb"},
+        {name:"2 bed apartment in Nice", image:"https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb"},
+        {name:"1 bed apartment in Cannes", image:"https://images.pexels.com/photos/276554/pexels-photo-276554.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb"}
+]
+
 
 app.get("/", function(req,res){
     res.render("landing")
 });
 
 app.get("/housing", function(req,res){
-    var housing = [
-        {name:"1 bed apartment in Barca", image:"https://images.pexels.com/photos/275484/pexels-photo-275484.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb"},
-        {name:"2 bed apartment in Nice", image:"https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb"},
-        {name:"1 bed apartment in Cannes", image:"https://images.pexels.com/photos/276554/pexels-photo-276554.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb"}
-    ]
+   
     
     res.render("housing", {housing:housing});
 })
+
+app.post("/housing", function(req,res){
+    // get data from form and add to housing array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newHouse = {name: name, image: image}
+    
+    housing.push(newHouse);
+    // redirect to housing page
+    res.redirect("/housing");
+});
+
+app.get("/housing/new", function(req,res){
+    res.render("new.ejs");
+});
 
 app.listen(process.env.PORT,process.env.IP, function (argument) {
    console.log("Yelpcamp app started");
